50efb6239a111644b55aca2bcf81cf7410f5c2fa
RamS24 <AlSaadi.Rami24@gmail.com>
2022-12-08 14:31:37 +0100

    first commit

---
+++ outline.js
@@ -0,0 +1,24 @@
+// Explain REST API with CRUD
+// - REST API is a set of functions and procedures that allow the creation of applications that access
+// the features or data of an operating system, application, or other service.
+// - REST API is a software/web-service that uses HTTP requests to GET, PUT, POST and DELETE data.
+// - CRUD stands for Create, Read, Update and Delete.
+// - CRUD is an acronym for the four basic functions of persistent storage.
+// - CRUD is a set of operations that can be performed on data stored in a database.
+// - Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment.
+
+// - Build a RESTFUL API
+// - Create a server with Express and Node.js.
+// - Create a REST API that can perform CRUD operations on a MongoDB database using Mongoose.
+
+// - Create a model
+// - Create a schema
+// - Create a controller
+// what is a conroller?
+// - A controller is a function that handles a request and returns a response.
+
+// - Create a route
+// - Create a server
+
+// ...
+
