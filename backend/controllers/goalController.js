675e3ac7b72b55bd85262e168ff2a3d59fceef6a
RamS24 <AlSaadi.Rami24@gmail.com>
2022-12-08 18:02:13 +0100

    add error Handler and async handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,35 +1,38 @@
+const asyncHandler = require('express-async-handler'); // description: https://www.npmjs.com/package/express-async-handler
+
 //@desc Get the goals
 //@route GET /api/goals
 //@access Private
-const getGoals = (req, res) => {
-    res.status(200).json({ message: 'Getting goals' });    
-}
+const getGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Getting goals' }); 
+})   
 //@desc Set a goal
 //@route POST /api/goals
 //@access Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
 
     if(!req.body.text) {
-        return res.status(400).json({ message: 'Please add a text field to your goal' })
+        res.status(400)
+        throw new Error('No goal text');
     }
 
     console.log(req.body);
 
     res.status(200).json({ message: 'Setting/Adding goal' });
-}
+})
 
 //@desc Update goal
 //@route PUT /api/goals/:id
 //@access Private
-const updateGoal =  (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Editing/Updating '+req.params.id});
-}
+})
 
 //@desc Delete goal
 //@route DELETE /api/goals/:id
 //@access Private
-const deleteGoal =  (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Remove/Delete goal!'+req.params.id });
-}
+})
 
 module.exports = { getGoals, setGoal, updateGoal, deleteGoal }
